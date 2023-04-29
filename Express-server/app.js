2a32492180d0e88d42364ab7877728d804a258eb
Kshitij-git24 <kshitijchaudhari12345@gmail.com>
2023-04-29 17:18:00 -0400

    bug fix in app.js

--- Express-server/app.js
+++ Express-server/app.js
@@ -5,15 +5,15 @@ const cors = require("cors");
 const session = require("express-session");
 const http = require("http");
 const { Server } = require("socket.io");
-const axios = require('axios')
+const axios = require("axios");
 
 app.use(cors());
 
 // Redis Client Connection
-const redis = require('redis');
+const redis = require("redis");
 const client = redis.createClient();
 (async () => {
-  client.on('error', (err) => console.log('Redis Client Error', err));
+  client.on("error", (err) => console.log("Redis Client Error", err));
   await client.connect();
 })();
 const server = http.createServer(app);
@@ -61,47 +61,47 @@ app.use("/Logout", (req, res, next) => {
   next();
 });
 
-
-app.get('/:genre/:number', async (req, res, next) => {
+app.get("/:genre/:number", async (req, res, next) => {
   const { genre, number } = req.params;
-  console.log(genre, number)
+  console.log(genre, number);
   const url = `http://localhost:5000/predict/${genre}/${number}`;
   try {
     const { data } = await axios.get(url);
     if (data.movies.length === 0) {
-      res.status(404).send('Movies not found');
+      res.status(404).send("Movies not found");
     } else {
-      allRecommendedMovies = []
+      allRecommendedMovies = [];
       await Promise.all(
-        data.movies.map(async (movie)=>{
+        data.movies.map(async (movie) => {
           let exists = await client.exists(movie);
           let movieObject = undefined;
-          if(exists){
-            let resultFromCache = await client.get(movie)
-            movieObject = JSON.parse(resultFromCache)
+          if (exists) {
+            let resultFromCache = await client.get(movie);
+            movieObject = JSON.parse(resultFromCache);
           } else {
-            const requestToGetMovie = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=d7acb997073d8ac06467543852351ad1&language=en-US&page=1&include_adult=false&query=${movie}`)
+            const requestToGetMovie = await axios.get(
+              `https://api.themoviedb.org/3/search/movie?api_key=d7acb997073d8ac06467543852351ad1&language=en-US&page=1&include_adult=false&query=${movie}`
+            );
             if (requestToGetMovie.data.results.length != 0) {
               movieObject = {
                 id: requestToGetMovie.data.results[0].id,
-                title:requestToGetMovie.data.results[0].original_title,
-                image: requestToGetMovie.data.results[0].backdrop_path
-              }
-              await client.set(movie, JSON.stringify(movieObject))
+                title: requestToGetMovie.data.results[0].original_title,
+                image: requestToGetMovie.data.results[0].backdrop_path,
+              };
+              await client.set(movie, JSON.stringify(movieObject));
             }
           }
-          allRecommendedMovies.push(movieObject)
+          allRecommendedMovies.push(movieObject);
         })
-      )
+      );
       return res.json(allRecommendedMovies);
     }
   } catch (err) {
     console.error(err);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send("Internal Server Error");
   }
   next();
 });
-
 
 configRoutes(app);
 
@@ -136,7 +136,7 @@ io.on("connection", (socket) => {
   });
 });
 //
-// io.origins("*:*");
+// io.origins(":");
 
 server.listen(8000, () => {
   console.log("We've now got a NODE server!");
