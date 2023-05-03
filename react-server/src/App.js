881b08253810f9f2eefe55eefa0b47c77278e104
Kshitij-git24 <kshitijchaudhari12345@gmail.com>
2023-05-02 20:06:21 -0400

    auth bugs fully fixed and chatroom functional

--- react-server/src/App.js
+++ react-server/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import "./App.css";
 import RegistrationForm from "./components/RegistrationForm";
 import Login from "./components/Login";
@@ -9,41 +9,39 @@ import Home from "./components/Home";
 import Genre from "./components/Genre";
 import Navbar from "./components/Navbar";
 import LoadMovie from "./components/LoadMovie";
-import UserContext, { AuthContext } from './UserContext';
-import { useLocation, useNavigate} from "react-router-dom";
+import UserContext, { AuthContext } from "./UserContext";
+import { useLocation, useNavigate } from "react-router-dom";
 
 //Gloabal Variable
 window.myGlobalUserId = null;
 
 function App() {
-  // let session_data_auth = localStorage.getItem("session_auth");
-  // let session_data_emailID = localStorage.getItem("session_email");
-  // const navigate = useNavigate();
-  // const location = useLocation();
-  // let session = location.state && location.state.user_session;
-  // console.log(session);
-  // let userId = session && session.uid;
-  // let email = session && session.emailID;
-  // let username = email && email.split("@").shift();
-  // console.log("Inside HOME", username);
-  // userId = userId && userId.toString(); 
+  const [currentUser] = useContext(AuthContext);
+  useEffect(() => {
+    console.log({ currentUser });
+    const access = () => {
+      if (currentUser === null) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  }, []);
 
-  // useEffect(() => {
-  //   if (session === null || localStorage.getItem("session_auth") == null) {
-  //     localStorage.clear();
-  //     navigate("/login", { state: { expired: true } });
-  //   }
-  // }, [session, localStorage.getItem("session_auth")]);
-  const [currentUser] = useContext(AuthContext)
-    useEffect(() => {
-      console.log({currentUser})
-  });
+  const access = () => {
+    if (currentUser === null) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
     <Router>
       <div className="app">
         <Navbar />
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={access() ? <Login /> : <Home />} />
           <Route path="/register" element={<RegistrationForm />} />
           {/* The Routes from here on will be protected Routes. All routes expect Login and Register and in futuer Forgot password will be only non protected routes */}
 
