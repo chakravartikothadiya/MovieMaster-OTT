333c58a1fe3d0af55081b9ec3d2b89b384dfab3e
Kshitij-git24 <kshitijchaudhari12345@gmail.com>
2023-05-08 16:43:48 -0400

    logs deleted

--- Express-server/firebase/db.js
+++ Express-server/firebase/db.js
@@ -2,7 +2,6 @@ const admin = require("firebase-admin");
 require("firebase/auth");
 require("firebase/firestore");
 const credentials = require("./credentials.json");
-console.log("Inside databse config file");
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
 });
--- Express-server/routes/formInputs.js
+++ Express-server/routes/formInputs.js
@@ -16,8 +16,6 @@ let firebase_auth = {};
 
 router.route("/RegistrationForm").post(async (req, res) => {
   try {
-    // console.log("in RegistrationForm");
-    // console.log(req.body);
     let userResponse;
     //creating user and initially setting the verification to be false
 
@@ -25,7 +23,6 @@ router.route("/RegistrationForm").post(async (req, res) => {
       .collection("users")
       .where("email", "==", req.body.email)
       .get();
-    // console.log(getUserEmail);
     if (req.body.password.length < 6) {
       return res.json({
         message: "password length should be greater than or equal to 6",
@@ -35,16 +32,14 @@ router.route("/RegistrationForm").post(async (req, res) => {
       return res.json({ message: "email already exists" });
 
     if (getUserEmail.empty) {
-      // console.log("1");
       userResponse = await admin.auth().createUser({
         email: req.body.email,
         password: req.body.password,
         emailVerified: false,
         disabled: false,
       });
-      // console.log(2);
     }
-    // console.log(userResponse);
+
     if (userResponse) {
       // Inserting user in firebase collection
       const userJson = {
@@ -56,21 +51,15 @@ router.route("/RegistrationForm").post(async (req, res) => {
         .collection("users")
         .doc(req.body.email)
         .set(userJson);
-
-      // if (response) console.log(true);
     }
     res.json(userResponse);
   } catch (e) {
-    // res.status(e.statusCode || 500).json(e);
     res.json(e);
   }
 });
 
 router.route("/LoginForm").post(async (req, res) => {
   try {
-    // console.log(user_session);
-    console.log("in LoginForm");
-
     const getUser = await db
       .collection("users")
       .where("email", "==", req.body.email)
@@ -80,27 +69,22 @@ router.route("/LoginForm").post(async (req, res) => {
     const password = req.body.password;
 
     const auth = getAuth(app);
-    // console.log(`before`, auth.currentUser);
     const userCredential = await signInWithEmailAndPassword(
       auth,
       req.body.email,
       password
     );
     const user = userCredential.user;
-    // console.log("after", auth.currentUser);
     if (!user) {
       res.json("no user found");
     }
-    // console.log(req);
     req.session.uid = user.uid;
     req.session.emailID = req.body.email;
     req.session.login = true;
     req.session.save();
 
     firebase_auth = { user };
-    // console.log("SESSION", req.session.id);
     user_session = req.session;
-    console.log("User_session", user_session);
     res.json(req.session);
     // res.json(user);
   } catch (e) {
@@ -110,16 +94,11 @@ router.route("/LoginForm").post(async (req, res) => {
 
 router.route("/Logout").post(async (req, res) => {
   try {
-    console.log("inside Logout route");
-    // console.log(req.session.id);
-    console.log(user_session);
     user_session = {};
     const auth = getAuth(app);
-    // console.log("in logout", await auth.currentUser());
     auth
       .signOut()
       .then((result) => {
-        console.log(result);
         console.log("User has been signed out successfully");
         res.json({ message: "user logged out" });
       })
@@ -134,12 +113,9 @@ router.route("/Logout").post(async (req, res) => {
 
 router.route("/ResetPassword").post(async (req, res) => {
   try {
-    console.log("in ResetPassword");
-    console.log(req.body.email);
     // Send a password reset email to the user's email address
     const auth = getAuth(app);
     const resp = await sendPasswordResetEmail(auth, req.body.email);
-    // console.log(resp);
     res.json({ message: "Password reset email sent" });
   } catch (e) {
     res.json(e);
